4004d8b6ec8d161854179da4774e8e6af444dda6
fonsski <afonintens@gmail.com>
2024-12-16 11:35:54 +0600

    Add a needful json, add missing city

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/WeatherProcessor.php';
 
 $template = file_get_contents(__DIR__ . '/templates/form.html');
 echo $template;
